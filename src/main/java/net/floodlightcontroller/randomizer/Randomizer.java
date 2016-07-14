3b4b096ef5e34f196a8a4d75fa0e79195b9044e6
Geddings Barrineau <cbarrin@g.clemson.edu>
2016-07-14 16:02:49 -0400

    First commit! Basic module necessities are all that are in this commit. Implementations and registrations.

---
+++ src/main/java/net/floodlightcontroller/randomizer/Randomizer.java
@@ -0,0 +1,96 @@
+package net.floodlightcontroller.randomizer;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by geddingsbarrineau on 7/14/16.
+ */
+public class Randomizer implements IOFMessageListener, IFloodlightModule{
+
+    //================================================================================
+    //region Properties
+    private IFloodlightProviderService floodlightProvider;
+    private static Logger log;
+    //endregion
+    //================================================================================
+
+
+    //================================================================================
+    //region IOFMessageListener Implementation
+    @Override
+    public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return Randomizer.class.getSimpleName();
+    }
+
+    @Override
+    public boolean isCallbackOrderingPrereq(OFType type, String name) {
+        return false;
+    }
+
+    @Override
+    public boolean isCallbackOrderingPostreq(OFType type, String name) {
+        if (type.equals(OFType.PACKET_IN) && (name.equals("forwarding"))) {
+            log.trace("Randomizer is telling Forwarding to run later.");
+            return true;
+        } else {
+            return false;
+        }
+    }
+    //endregion
+    //================================================================================
+
+
+    //================================================================================
+    //region IFloodlightModule Implementation
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+        return null;
+    }
+
+    @Override
+    public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+        return null;
+    }
+
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+        Collection<Class<? extends IFloodlightService>> l = new ArrayList<>();
+        l.add(IFloodlightProviderService.class);
+        return l;
+    }
+
+    @Override
+    public void init(FloodlightModuleContext context) throws FloodlightModuleException {
+        floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+        log = LoggerFactory.getLogger(Randomizer.class);
+    }
+
+    @Override
+    public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
+        floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+    }
+    //endregion
+    //================================================================================
+
+
+}
